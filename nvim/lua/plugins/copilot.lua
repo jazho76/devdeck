307205a772a9eafd49730eeaf51f600b6de356f7
joaquin <jpinilloslr@gmail.com>
2026-05-25 19:19:46 +0200

    fix copilot

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -2,6 +2,10 @@ return {
   'github/copilot.vim',
   config = function()
     vim.g.copilot_assume_mapped = true
-    vim.keymap.set('i', '<C-S>', 'copilot#Accept("<CR>")', { silent = true, expr = true })
+    vim.keymap.set('i', '<C-S>', 'copilot#Accept("\\<CR>")', {
+      silent = true,
+      expr = true,
+      replace_keycodes = false,
+    })
   end,
 }
