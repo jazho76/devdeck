d9c92c1d04983170515f49ab0155aa86f2e1a5d9
joaquin <jpinilloslr@gmail.com>
2026-05-25 18:43:04 +0200

    add script to configure toolsets

--- nvim/lua/config/languages.lua
+++ nvim/lua/config/languages.lua
@@ -1,11 +1,6 @@
-return {
-  'docs',
-  'config',
-  'python',
-  'web',
-  'asm',
-  'go',
-  'rust',
-  'csharp',
-  'c',
-}
+local ok, override = pcall(require, 'config.languages-local')
+if ok and type(override) == 'table' then
+  return override
+end
+
+return require('config.languages-default')
