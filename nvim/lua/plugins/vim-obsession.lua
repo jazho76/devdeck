944de077646621e30a4a11f02747d4944c511d29
joaquin <jpinilloslr@gmail.com>
2026-05-26 17:40:12 +0200

    add resurrect and vim obsession

--- nvim/lua/plugins/vim-obsession.lua
+++ nvim/lua/plugins/vim-obsession.lua
@@ -1,4 +1,6 @@
 return {
-  -- For easy session management, this integrates with tmux-resurrect
   'tpope/vim-obsession',
+  config = function()
+    vim.keymap.set('n', '<leader>o', '<cmd>Obsession<CR>', { desc = 'Toggle [O]bsession session' })
+  end,
 }
