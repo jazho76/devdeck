c2f13627826a975e987930692f699656d0123ca4
joaquin <jpinilloslr@gmail.com>
2026-05-29 23:45:28 +0200

    move obsession to toggles

--- nvim/lua/plugins/vim-obsession.lua
+++ nvim/lua/plugins/vim-obsession.lua
@@ -1,6 +1,6 @@
 return {
   'tpope/vim-obsession',
   config = function()
-    vim.keymap.set('n', '<leader>o', '<cmd>Obsession<CR>', { desc = 'Toggle Obsession session' })
+    vim.keymap.set('n', '<leader>to', '<cmd>Obsession<CR>', { desc = 'Toggle Obsession session' })
   end,
 }
