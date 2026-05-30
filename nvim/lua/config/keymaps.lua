7266f98d6cc9ed3e5a06e6fcadc24c86e95a4b33
joaquin <jpinilloslr@gmail.com>
2026-05-30 13:53:59 +0200

    update <C-o> to close everything else

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -14,10 +14,9 @@ vim.keymap.set('n', '<A-o>', function()
   local current = vim.api.nvim_get_current_buf()
   for _, buf in ipairs(vim.api.nvim_list_bufs()) do
     if buf ~= current
-      and vim.bo[buf].buflisted
-      and vim.bo[buf].buftype == ''
+      and vim.api.nvim_buf_is_valid(buf)
       and not vim.bo[buf].modified then
-      vim.api.nvim_buf_delete(buf, {})
+      vim.api.nvim_buf_delete(buf, { force = true })
     end
   end
 end, { noremap = true, silent = true, desc = 'Close all buffers except the current buffer' })
