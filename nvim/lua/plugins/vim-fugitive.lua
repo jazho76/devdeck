b4ffbcdbca05e9620b52c4ecea106a6677039632
joaquin <jpinilloslr@gmail.com>
2026-05-30 13:54:17 +0200

    add fugitive commands

--- nvim/lua/plugins/vim-fugitive.lua
+++ nvim/lua/plugins/vim-fugitive.lua
@@ -2,5 +2,9 @@ return {
   'tpope/vim-fugitive',
   config = function()
     vim.keymap.set('n', '<leader>gg', ':G | only<CR>', { desc = 'Open Fugitive status' })
+    vim.keymap.set('n', '<leader>gp', ':Git push<CR>', { desc = 'Git push' })
+    vim.keymap.set('n', '<leader>gP', ':Git pull<CR>', { desc = 'Git pull' })
+    vim.keymap.set('n', '<leader>gF', ':Git fetch<CR>', { desc = 'Git fetch' })
+    vim.keymap.set('n', '<leader>gB', ':Git blame<CR>', { desc = 'Git blame' })
   end
 }
