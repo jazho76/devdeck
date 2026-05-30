7af159d77d7ea0a8e8cde8b4786d55d5274ea370
joaquin <jpinilloslr@gmail.com>
2026-05-30 15:33:00 +0200

    update async git fugitive commands

--- nvim/lua/plugins/vim-fugitive.lua
+++ nvim/lua/plugins/vim-fugitive.lua
@@ -1,10 +1,10 @@
 return {
   'tpope/vim-fugitive',
   config = function()
-    vim.keymap.set('n', '<leader>gg', ':G | only<CR>', { desc = 'Open Fugitive status' })
-    vim.keymap.set('n', '<leader>gp', ':Git push<CR>', { desc = 'Git push' })
-    vim.keymap.set('n', '<leader>gP', ':Git pull<CR>', { desc = 'Git pull' })
-    vim.keymap.set('n', '<leader>gF', ':Git fetch<CR>', { desc = 'Git fetch' })
+    vim.keymap.set('n', '<leader>gg', ':Git | only<CR>', { desc = 'Open Fugitive status' })
+    vim.keymap.set('n', '<leader>gp', ':Git! push<CR>', { desc = 'Git push' })
+    vim.keymap.set('n', '<leader>gP', ':Git! pull<CR>', { desc = 'Git pull' })
+    vim.keymap.set('n', '<leader>gF', ':Git! fetch<CR>', { desc = 'Git fetch' })
     vim.keymap.set('n', '<leader>gB', ':Git blame<CR>', { desc = 'Git blame' })
     local function browse(range)
       local remote = vim.env.SSH_TTY or vim.env.SSH_CONNECTION
@@ -12,6 +12,7 @@ return {
       vim.cmd((range and "'<,'>" or '') .. cmd)
     end
     vim.keymap.set('n', '<leader>go', function() browse(false) end, { desc = 'Open git web URL (copy when remote)' })
-    vim.keymap.set('x', '<leader>go', function() browse(true) end, { desc = 'Open git web URL for selection (copy when remote)' })
+    vim.keymap.set('x', '<leader>go', function() browse(true) end,
+      { desc = 'Open git web URL for selection (copy when remote)' })
   end
 }
