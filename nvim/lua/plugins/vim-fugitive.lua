ff3ea0431cd4c7c3b34c376f04154ff2c5530205
joaquin <jpinilloslr@gmail.com>
2026-05-30 17:50:05 +0200

    maximaze fugitive diffs automatically

--- nvim/lua/plugins/vim-fugitive.lua
+++ nvim/lua/plugins/vim-fugitive.lua
@@ -14,5 +14,19 @@ return {
     vim.keymap.set('n', '<leader>go', function() browse(false) end, { desc = 'Open git web URL (copy when remote)' })
     vim.keymap.set('x', '<leader>go', function() browse(true) end,
       { desc = 'Open git web URL for selection (copy when remote)' })
+
+    -- Make `dv` in the status buffer open the diff and maximize it
+    vim.api.nvim_create_autocmd('FileType', {
+      pattern = 'fugitive',
+      callback = function(ev)
+        -- Defer so this wins over fugitive's own ftplugin `dv` mapping
+        vim.schedule(function()
+          if vim.api.nvim_buf_is_valid(ev.buf) then
+            vim.keymap.set('n', 'dv', '<Plug>fugitive:dv<C-w>_',
+              { buffer = ev.buf, remap = true, desc = 'Vertical diff split (maximized height)' })
+          end
+        end)
+      end,
+    })
   end
 }
