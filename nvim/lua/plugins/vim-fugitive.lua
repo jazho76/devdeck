23ea39f4951d18db56eeb318518894bc8f42ef5f
joaquin <jpinilloslr@gmail.com>
2026-06-07 01:44:00 +0200

    update git pull/push

--- nvim/lua/plugins/vim-fugitive.lua
+++ nvim/lua/plugins/vim-fugitive.lua
@@ -2,9 +2,9 @@ return {
   'tpope/vim-fugitive',
   config = function()
     vim.keymap.set('n', '<leader>gg', ':Git | only<CR>', { desc = 'Open Fugitive status' })
-    vim.keymap.set('n', '<leader>gp', ':Git! push<CR>', { desc = 'Git push' })
-    vim.keymap.set('n', '<leader>gP', ':Git! pull<CR>', { desc = 'Git pull' })
-    vim.keymap.set('n', '<leader>gF', ':Git! fetch<CR>', { desc = 'Git fetch' })
+    vim.keymap.set('n', '<leader>gp', ':Git push<CR>', { desc = 'Git push' })
+    vim.keymap.set('n', '<leader>gP', ':Git pull<CR>', { desc = 'Git pull' })
+    vim.keymap.set('n', '<leader>gF', ':Git fetch<CR>', { desc = 'Git fetch' })
     vim.keymap.set('n', '<leader>gB', ':Git blame<CR>', { desc = 'Git blame' })
     local function browse(range)
       local remote = vim.env.SSH_TTY or vim.env.SSH_CONNECTION
