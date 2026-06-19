5402c4190e019e577947bc4dcb550736874b4106
Matthias Keller <ghost1338.secops@gmail.com>
2026-06-19 06:42:00 +0000

    fix: remember nvim-tree width when opening files

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -11,7 +11,20 @@ return {
     }
   },
   config = function()
+    local api = require('nvim-tree.api')
+    local Event = api.events.Event
     local remembered_tree_width
+
+    local function remember_tree_width(tree_window)
+      tree_window = tree_window or api.tree.winid()
+      if tree_window and vim.api.nvim_win_is_valid(tree_window) then
+        remembered_tree_width = vim.api.nvim_win_get_width(tree_window)
+      end
+    end
+
+    local function restore_tree_width()
+      api.tree.resize({ absolute = remembered_tree_width or default_tree_width() })
+    end
 
     require('nvim-tree').setup {
       update_focused_file = {
@@ -28,19 +41,14 @@ return {
       }
     }
 
-    local api = require('nvim-tree.api')
-    local Event = api.events.Event
-
-    local function remember_tree_width()
-      local tree_window = api.tree.winid()
-      if tree_window and vim.api.nvim_win_is_valid(tree_window) then
-        remembered_tree_width = vim.api.nvim_win_get_width(tree_window)
-      end
-    end
-
-    local function restore_tree_width()
-      api.tree.resize({ absolute = remembered_tree_width or default_tree_width() })
-    end
+    api.events.subscribe(Event.TreeOpen, restore_tree_width)
+    vim.api.nvim_create_autocmd('WinLeave', {
+      callback = function(event)
+        if vim.bo[event.buf].filetype == 'NvimTree' then
+          remember_tree_width(vim.api.nvim_get_current_win())
+        end
+      end,
+    })
 
     local function toggle_tree()
       if api.tree.is_visible() then
@@ -48,11 +56,8 @@ return {
         api.tree.close()
       else
         api.tree.open()
-        restore_tree_width()
       end
     end
-
-    api.events.subscribe(Event.TreeClose, remember_tree_width)
 
     vim.keymap.set('n', '<A-e>', toggle_tree, { noremap = true, silent = true, desc = 'Toggle the NvimTree file explorer' })
   end,
